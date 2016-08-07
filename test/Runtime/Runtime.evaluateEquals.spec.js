8cec56d2e936d68dcc2c6a72e33f5c9c9fd4faee
Tim Oram <mitmaro@gmail.com>
2016-08-06 21:57:18 -0230

    Added tests for non-float evaluateEquals values

--- test/Runtime/Runtime.evaluateEquals.spec.js
+++ test/Runtime/Runtime.evaluateEquals.spec.js
@@ -4,17 +4,33 @@ const expect = require('chai').expect;
 const Runtime = require('../../src/Runtime');
 
 describe('Runtime.evaluateEquals', function() {
-	it('should get matching input data', function() {
+	it('should compare same loose equals on non-float', function() {
 		const runtime = new Runtime();
 
-		runtime.input = { foo: 'bar' };
-		expect(runtime.getValueFromVariable('foo')).to.equal('bar');
+		expect(runtime.evaluateEquals(1, true)).to.be.true;
 	});
 
-	it('should error on missing input data', function() {
+	it('should compare different loose equals on non-float', function() {
 		const runtime = new Runtime();
 
-		runtime.input = {};
-		expect(() => runtime.getValueFromVariable('foo')).to.throw(/Unset variable/);
+		expect(runtime.evaluateEquals(0, true)).to.be.false;
+	});
+
+	it('should compare same strict equals on non-float', function() {
+		const runtime = new Runtime();
+
+		expect(runtime.evaluateEquals('foo', 'foo', true)).to.be.true;
+	});
+
+	it('should compare different types strict equals on non-float', function() {
+		const runtime = new Runtime();
+
+		expect(runtime.evaluateEquals(true, 1, true)).to.be.false;
+	});
+
+	it('should compare different values strict equals on non-float', function() {
+		const runtime = new Runtime();
+
+		expect(runtime.evaluateEquals('foo', 'bar', true)).to.be.false;
 	});
 });
